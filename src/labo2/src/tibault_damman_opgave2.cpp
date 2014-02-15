f02d6aaac893d64166778dd28313bb9cfe3eb1e0
Tibault Damman <tibault.damman@gmail.com>
2014-02-15 22:40:43 +0100

    pimped my CMakeLists + made lab2

--- src/labo2/src/tibault_damman_opgave2.cpp
+++ src/labo2/src/tibault_damman_opgave2.cpp
@@ -5,8 +5,48 @@
 
 using namespace cv;
 
+const char* win1 = "input";
+const char* win2 = "fixed";
+
+void start_show(const char* filename)
+{
+    namedWindow(win1);
+    namedWindow(win2);
+    
+	Mat img = imread(filename);
+	
+	Mat blur;
+	Size ksize;
+	ksize.width = 5;
+	ksize.height = 5;
+	GaussianBlur(img, blur, ksize, 1.2);
+    
+    imshow(win1, img);
+    imshow(win2, blur);
+	waitKey();
+}
+
 int main(int argc, char **argv){
-	printf("Lege template voor labo beeldverwerking: %s\n",argv[0]);
+	printf("labo beeldverwerking - opdracht 2\n");
+	
+	if (argc > 1)
+	{
+	    printf("show: %s\n",argv[1]);
+	    
+	    try 
+	    {
+		    start_show(argv[1]);
+	    }
+	    catch (...)
+	    {
+	        printf("Something fucked up\n");
+	        return 1;
+	    }
+	}
+	else
+	{
+	    printf("usage: %s <image>\n",argv[0]);
+	}
 
 	return 0;
 }
